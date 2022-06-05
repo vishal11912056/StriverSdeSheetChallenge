252240b9b35a152bcdc7f14c710b9fcebce0a894
Vishal Prajapati <60194098+vishal11912056@users.noreply.github.com>
2022-06-05 19:29:05 +0530

    Update Find Minimum Number Of Coins.cpp

--- Greedy Algorithm/Find Minimum Number Of Coins.cpp
+++ Greedy Algorithm/Find Minimum Number Of Coins.cpp
@@ -1,1 +1,21 @@
+int findMinimumCoins(int amount) 
+{
+	int arr[]={1, 2, 5, 10, 20, 50, 100, 500, 1000};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	int ans=0;
+	for(int i=n-1;i>=0;i--)
+	{
+		while(amount>=arr[i])
+		{
+			ans++;
+			amount-=arr[i];
+		}
+		if(amount<=0)
+		{
+			break;
+		}
+	}
+	return ans;
+    // Write your code here
+}
 
