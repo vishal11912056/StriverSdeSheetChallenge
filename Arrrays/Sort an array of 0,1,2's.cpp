edd04d29565128a3289497d428fb489494cf345b
Vishal Prajapati <60194098+vishal11912056@users.noreply.github.com>
2022-06-05 17:48:59 +0530

    Create Sort an array of 0,1,2's.cpp

---
+++ Arrrays/Sort an array of 0,1,2's.cpp
@@ -0,0 +1,26 @@
+void sort012(int *nums, int n)
+{
+	int lo = 0; 
+        int hi = n - 1; 
+        int mid = 0; 
+
+        while (mid <= hi) { 
+            switch (nums[mid]) { 
+
+            // If the element is 0 
+            case 0: 
+                swap(nums[lo++], nums[mid++]); 
+                break; 
+
+            // If the element is 1 . 
+            case 1: 
+                mid++; 
+                break; 
+
+            // If the element is 2 
+            case 2: 
+                swap(nums[mid], nums[hi--]); 
+                break; 
+            }
+        }
+}
