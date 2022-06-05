b25f807cd0b03c844ad035b3f7c14a2ff94aaa85
Vishal Prajapati <60194098+vishal11912056@users.noreply.github.com>
2022-06-05 21:06:52 +0530

    Create Matrix Chain Multiplication.cpp

---
+++ Dynamic Programming/Matrix Chain Multiplication.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+int matrixMultiplication(vector<int> &arr, int N)
+{
+	vector<vector<int>> dp(N,vector<int>(N,0));
+	for(int i=N-1;i>=1;i--)
+	{
+		for(int j=i+1;j<N;j++)
+		{
+			
+			int mini=1e9;
+			for(int k=i;k<j;k++)
+			{
+				int step = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+				mini = min(mini,step);
+			}
+			dp[i][j]=mini;
+		}
+	}
+	return dp[1][N-1];
+}
