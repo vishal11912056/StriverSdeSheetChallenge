7ca604b760381c543077c8f8e276de039025b727
Vishal Prajapati <60194098+vishal11912056@users.noreply.github.com>
2022-06-07 16:25:34 +0530

    Update Find Pattern in String - KMP Algorithm.cpp

--- String-2/Find Pattern in String - KMP Algorithm.cpp
+++ String-2/Find Pattern in String - KMP Algorithm.cpp
@@ -1,1 +1,69 @@
+ bool rabincarp(string txt,string pattrn)
+    {
+        int d=31;
+        int q=101;
+        int m=pattrn.length();
+        int n= txt.length();
+        int p=0;
+        int t=0;
+        int h=1;
+        for(int i=0;i<m-1;i++)
+        {
+            h= (h*d)%q;
+        }
+        for(int i=0;i<m;i++)
+        {
+            p= (p*d + pattrn[i])%q;
+            t= (t*d + txt[i])%q;
+        }
+        for(int i=0;i<=(n-m);i++)
+        {
+            
+            if(p==t)
+            {
+               int j=0;
+                for(;j<m;j++)
+                {
+                    if(txt[i+j]!=pattrn[j])
+                    {
+                        break;
+                    }
+                }
+                if(j==m)
+                {
+                    return true;
+                }
+                
+            }
+            if(i< n-m)
+            {
+                t = (d*(t - txt[i]*h)%q+ txt[i+m])%q;
+                
+                if(t<0)
+                {
+                    t=t+q;
+                }
+            }
+            
+        }
+        return false;
+        
+    }
+    bool findPattern(string needle, string haystack) 
+    {
+        
+        int n= haystack.length();
+        int m= needle.length();
+        if(m==0)
+        {
+            return true;
+        }
+        if(m>n)
+        {
+            return false;
+        }
+        return rabincarp(haystack,needle);
+        // return -1;
+        
+    }
 
